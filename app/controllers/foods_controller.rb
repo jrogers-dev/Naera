7fcbbd7a4698c237e7816c665f3b6aa260697bd8
James Rogers <jarogers095@gmail.com>
2020-05-23 21:44:19 -0700

    Added route in FoodsController for deletion of a food

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,41 +1,19 @@
 class FoodsController < ApplicationController
-    get "/foods" do
+    delete "/foods/:id" do
         if logged_in?
-            @user = current_user
-            @foods = @user.foods
-            erb :"/foods/foods"
-        else
-            redirect "/login"
-        end
-    end
-
-    get "/foods/:id" do
-        if logged_in?
-            @user = current_user
             @food = Food.find(params[:id])
+            @user = current_user
             if @food.user_id == @user.id
-                erb :"/days/show1"
-            else 
-                redirect "/foods"
+                @day = Day.find(@food.day.id)
+                @food.destroy
+                redirect "/days/#{@day.id}"
+            else
+                redirect "/days/today"
             end
         else
             redirect "/login"
         end
     end
-
-    #post "/days/:id" do
-    #    @user = current_user
-    #    @day = Day.find(params[:id])
-    #    if params[:name] != ""
-    #        @food = Food.create(params[:food])
-    #        @user.foods << @food
-    #        @day.foods << @food
-    #        @day.update_stats
-    #        redirect "/days/#{@day.id}"
-    #    else
-    #        redirect "/days/#{@day.id}/edit"
-    #    end 
-    #end
 
     get "/foods/:id/edit" do
         if logged_in?
